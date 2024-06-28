1b5119ef87d73ad2a3e9b5155ad9387044c3ac0e
Denis Zagitov <deniszagitov@gmail.com>
2024-06-28 12:44:49 +0300

    let's show the news

--- news.php
+++ news.php
@@ -6,6 +6,10 @@ bd_connect();
 ini_set('display_errors', 1);
 error_reporting(E_ALL ^ E_NOTICE);
 error_reporting(E_ALL);
+
+// Извлечение новостей из базы данных
+$query = "SELECT news_id, title, content, DATE_FORMAT(published_date, '%d.%m.%Y') as published_date FROM news ORDER BY published_date DESC";
+$result = mysqli_query($db, $query);
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -27,7 +31,17 @@ error_reporting(E_ALL);
     <div class="page-content">
       <h3>Новости юридического мира</h3>
       <span class="paragraph-text">
-        <p>Страница в разработке</p>
+        <?php if (mysqli_num_rows($result) > 0) : ?>
+          <?php while ($row = mysqli_fetch_assoc($result)) : ?>
+            <div class="news-item">
+              <div class="news_date">Дата публикации: <?= $row['published_date'] ?></div>
+              <h4><a href="/news_detail.php?id=<?= $row['news_id'] ?>"><?= $row['title'] ?></a></h4>
+              <p><?= $row['content']?></p>
+            </div>
+          <?php endwhile; ?>
+        <?php else : ?>
+          <p>На данный момент новостей нет.</p>
+        <?php endif; ?>
       </span>
     </div>
   </div>
--- read.php
+++ read.php
@@ -3,10 +3,14 @@
 // Содержит компоненты для загрузки шаблонов договоров на компьютер
 session_start();
 include("common/func.php");
-bd_connect();
+$conn = bd_connect(); // Установка соединения с базой данных
+
 ini_set('display_errors', 1);
 error_reporting(E_ALL ^ E_NOTICE);
-error_reporting(E_ALL);
+
+// Проверка на авторизацию пользователя
+$authorized = isset($_SESSION['name_client']);
+
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -26,54 +30,7 @@ error_reporting(E_ALL);
     <?php include("common/top1.php"); ?>
     <div class="page-content">
       <div class="center-content">
-        <h3>Загрузка шаблонов договоров</h3>
-        <?php
-        if (isset($_FILES['inputfile'])) {
-          $destination_dir = __DIR__ . '/dogs/new/' . basename($_FILES['inputfile']['name']);
-          if (move_uploaded_file($_FILES['inputfile']['tmp_name'], $destination_dir)) {
-            echo "<p>Файл успешно загружен!</p>";
-          } else {
-            echo "<p>Ошибка при загрузке файла!</p>";
-            switch ($_FILES['inputfile']['error']) {
-              case UPLOAD_ERR_INI_SIZE:
-                echo "<p>Размер файла превышает допустимый лимит.</p>";
-                break;
-              case UPLOAD_ERR_FORM_SIZE:
-                echo "<p>Размер файла превышает указанный лимит в HTML-форме.</p>";
-                break;
-              case UPLOAD_ERR_PARTIAL:
-                echo "<p>Файл был загружен только частично.</p>";
-                break;
-              case UPLOAD_ERR_NO_FILE:
-                echo "<p>Файл не был загружен.</p>";
-                break;
-              case UPLOAD_ERR_NO_TMP_DIR:
-                echo "<p>Отсутствует временная директория.</p>";
-                break;
-              case UPLOAD_ERR_CANT_WRITE:
-                echo "<p>Не удалось записать файл на диск.</p>";
-                break;
-              case UPLOAD_ERR_EXTENSION:
-                echo "<p>PHP-расширение остановило загрузку файла.</p>";
-                break;
-              default:
-                echo "<p>Неизвестная ошибка.</p>";
-                break;
-            }
-          }
-        }
-        ?>
-
-        <form enctype='multipart/form-data' action='read.php' method="post" class="upload-form">
-          <div class="form-group">
-            <label for="inputfile"><span class="paragraph-text">Сохранить файл</span></label>
-            <input class="form-control-file" type="file" id="inputfile" name="inputfile">
-          </div>
-          <div class="form-group">
-            <input class="btn-new" type="submit" value="Нажмите для сохранения">
-          </div>
-        </form>
-
+        <h3>Скачать шаблон договора</h3>
         <div class="doc-image-container">
           <div class="doc-item">
             <a href="/dogs/Договор на сопровождение исполнительного производства по земельно-имущественным спорам.docx" download>
@@ -94,6 +51,56 @@ error_reporting(E_ALL);
             <p>Шаблон договора оказания юридических услуг в пользу исполнителя</p>
           </div>
         </div>
+        <?php if ($authorized) : ?>
+          <?php
+          if (isset($_FILES['inputfile'])) {
+            $destination_dir = __DIR__ . '/dogs/new/' . basename($_FILES['inputfile']['name']);
+            if (move_uploaded_file($_FILES['inputfile']['tmp_name'], $destination_dir)) {
+              echo "<p>Файл успешно загружен!</p>";
+            } else {
+              echo "<p>Ошибка при загрузке файла!</p>";
+              switch ($_FILES['inputfile']['error']) {
+                case UPLOAD_ERR_INI_SIZE:
+                  echo "<p>Размер файла превышает допустимый лимит.</p>";
+                  break;
+                case UPLOAD_ERR_FORM_SIZE:
+                  echo "<p>Размер файла превышает указанный лимит в HTML-форме.</p>";
+                  break;
+                case UPLOAD_ERR_PARTIAL:
+                  echo "<p>Файл был загружен только частично.</p>";
+                  break;
+                case UPLOAD_ERR_NO_FILE:
+                  echo "<p>Файл не был загружен.</p>";
+                  break;
+                case UPLOAD_ERR_NO_TMP_DIR:
+                  echo "<p>Отсутствует временная директория.</p>";
+                  break;
+                case UPLOAD_ERR_CANT_WRITE:
+                  echo "<p>Не удалось записать файл на диск.</p>";
+                  break;
+                case UPLOAD_ERR_EXTENSION:
+                  echo "<p>PHP-расширение остановило загрузку файла.</p>";
+                  break;
+                default:
+                  echo "<p>Неизвестная ошибка.</p>";
+                  break;
+              }
+            }
+          }
+          ?>
+
+          <form enctype='multipart/form-data' action='read.php' method="post" class="upload-form">
+            <div class="form-group">
+              <label for="inputfile"><span class="paragraph-text">Загрузить заполненный шаблон договора</span></label>
+              <input class="form-control-file" type="file" id="inputfile" name="inputfile">
+            </div>
+            <div class="form-group">
+              <input class="btn-new" type="submit" value="Нажмите для сохранения">
+            </div>
+          </form>
+        <?php else : ?>
+          <p></p>
+        <?php endif; ?>
       </div>
     </div>
   </div>
