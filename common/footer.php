7a599e2eb23f8f0190aa3a5155cc59c58fb3948e
Denis Zagitov <deniszagitov@gmail.com>
2024-07-03 11:43:53 +0300

    All signed docs added

--- common/footer.php
+++ common/footer.php
@@ -1,5 +1,6 @@
 <footer class="denis-color text-white text-center fs-3">
         Удачного сотрудничества!
+        <p>&copy; 2024 TopLegalConsulting. Все права защищены.</p>
 </footer>
 
 <script src="/js/popper.min.js"></script>
